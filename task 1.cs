9609228410768af787a9c49241bfba44ce54baf1
hagwen <marius.hagen0512@gmail.com>
2024-01-24 17:37:28 +0100

    lagt inn ferdig oppgave

--- task 1.cs
+++ task 1.cs
@@ -1,26 +1,31 @@
-Console.WriteLine("helloe");
+int onemove = 0;
 
+
+
+
+//Console.WriteLine(onemove);
 
 for (int i = 0; i < 37; i++)
 {
+
     if (Peek())
     {
         Move();
+
     }
+
 } //14
+Console.WriteLine(onemove);
 
 Turn();
+Console.WriteLine("turnright.");
 Move();
 Move();
+Console.WriteLine(onemove);
 Turn(); //12
+Console.WriteLine("turnright.");
 
-void TurnLeft()
-{
-    for (int turns = 0; turns < 3; turns++)
-    {
-        Turn();
-    }
-}
+
 
 for (int k = 0; k < 36; k++)
 {
@@ -30,6 +35,7 @@ for (int k = 0; k < 36; k++)
     }
     //Turn(); 
 }
+Console.WriteLine(onemove);
 
 TurnLeft();
 
@@ -40,6 +46,7 @@ for (int m = 0; m < 2; m++)
         Move();
     }
 }
+Console.WriteLine(onemove);
 TurnLeft(); //10
 
 for (int k = 0; k < 36; k++)
@@ -48,8 +55,11 @@ for (int k = 0; k < 36; k++)
     {
         Move();
     }
-    Turn();
 }
+Console.WriteLine(onemove);
+Turn();
+Console.WriteLine("turnright.");
+
 for (int m = 0; m < 2; m++)
 {
     if (Peek())
@@ -57,8 +67,10 @@ for (int m = 0; m < 2; m++)
         Move();
     }
 }
+Console.WriteLine(onemove);
 
 Turn(); //8
+Console.WriteLine("turnright.");
 
 for (int k = 0; k < 36; k++)
 {
@@ -68,6 +80,7 @@ for (int k = 0; k < 36; k++)
     }
     //Turn(); 
 }
+Console.WriteLine(onemove);
 TurnLeft();
 
 for (int q = 0; q < 2; q++)
@@ -77,6 +90,8 @@ for (int q = 0; q < 2; q++)
         Move();
     }
 } //6
+Console.WriteLine(onemove);
+
 TurnLeft();
 
 for (int k = 0; k < 36; k++)
@@ -85,8 +100,11 @@ for (int k = 0; k < 36; k++)
     {
         Move();
     }
-    Turn();
 }
+Console.WriteLine(onemove);
+Turn();
+Console.WriteLine("turnright.");
+
 
 for (int u = 0; u < 2; u++)
 {
@@ -95,6 +113,7 @@ for (int u = 0; u < 2; u++)
         Move();
     }
 } //4
+Console.WriteLine(onemove);
 
 for (int k = 0; k < 36; k++)
 {
@@ -104,6 +123,8 @@ for (int k = 0; k < 36; k++)
     }
     //Turn();
 }
+Console.WriteLine(onemove);
+
 TurnLeft();
 
 for (int u = 0; u < 2; u++)
@@ -113,8 +134,9 @@ for (int u = 0; u < 2; u++)
         Move();
     }
 } //2
-
+Console.WriteLine(onemove);
 Turn();
+Console.WriteLine("turnright.");
 
 
 for (int l = 0; l < 38; l++)
@@ -122,10 +144,24 @@ for (int l = 0; l < 38; l++)
     if (Peek())
     {
         Move();
+        Console.WriteLine(onemove);
+
+        if (AtGoal())
+        {
+            Console.WriteLine("yeeeey, winner winner chicken dinner.");
+            return;
+        }
     }
-    else if (AtGoal())
+}
+//Console.WriteLine(onemove);
+
+//my functions
+void TurnLeft()
+{
+    Console.WriteLine("Turned left");
+    for (int turns = 0; turns < 3; turns++)
     {
-        //WIN
+        Turn();
     }
 }
 
@@ -135,12 +171,16 @@ for (int l = 0; l < 38; l++)
 
 void Move()
 {
-    // Moves the car 1 cell in the direction it is heading. 
+
+    // Moves the car 1 cell in the direction it is heading.
+    onemove++;
+
 }
 
 void Turn()
 {
     // Turns the car 90 deg clockwise.
+
 }
 
 bool Peek()
